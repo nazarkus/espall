69bc375af1eb6048fe521e10440882c6b13c58fb
nazarkus <nazarkuus@gmail.com>
2026-01-07 19:02:15 +0200

    Update espall.lua

--- espall.lua
+++ espall.lua
@@ -6,3 +6,4 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/droneesp/ref
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/ac130esp/refs/heads/main/ac130esp.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/boatesp/refs/heads/main/boatesp.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/hovercraftesp/refs/heads/main/hovercraftesp.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/vehicleesp/refs/heads/main/vehicleesp.lua"))()
